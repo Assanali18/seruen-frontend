504a4fd21d8df75ef2f6862924c63bbe7622061e
Assanali18 <ustaevasanali@gmail.com>
2024-08-03 08:38:18 +0500

    add spinner

--- client/src/app/components/GoogleMaps.tsx
+++ client/src/app/components/GoogleMaps.tsx
@@ -35,9 +35,9 @@ const getMarkerIconUrl = (eventDate: string) => {
     const daysUntilEvent = (eventDay.getTime() - today.getTime()) / (1000 * 3600 * 24);
 
     if (daysUntilEvent <= 2) {
-        return '/markers/yellow.png'; // Зеленый маркер
+        return '/markers/green.png'; // Зеленый маркер
     } else if (daysUntilEvent > 2 && daysUntilEvent <= 10) {
-        return '/markers/green.png'; // Желтый маркер
+        return '/markers/yellow.png'; // Желтый маркер
     } else {
         return '/markers/red.png'; // Красный маркер
     }
@@ -127,12 +127,91 @@ export default function GoogleMaps() {
             const { Map, Marker } = google.maps;
 
             const options: google.maps.MapOptions = {
-                center: userLocation,
+                center: userLocation || { lat: 39.60128890889341, lng: -9.069839810859907 },
                 zoom: 12,
                 mapTypeControl: false,
                 fullscreenControl: false,
                 streetViewControl: false,
-                styles: [ /* Ваши стили для карты */ ],
+                styles: [
+                    { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+                    { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+                    { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+                    {
+                        featureType: 'administrative.locality',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#d59563' }],
+                    },
+                    {
+                        featureType: 'poi',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#d59563' }],
+                    },
+                    {
+                        featureType: 'poi.park',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#263c3f' }],
+                    },
+                    {
+                        featureType: 'poi.park',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#6b9a76' }],
+                    },
+                    {
+                        featureType: 'road',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#38414e' }],
+                    },
+                    {
+                        featureType: 'road',
+                        elementType: 'geometry.stroke',
+                        stylers: [{ color: '#212a37' }],
+                    },
+                    {
+                        featureType: 'road',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#9ca5b3' }],
+                    },
+                    {
+                        featureType: 'road.highway',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#746855' }],
+                    },
+                    {
+                        featureType: 'road.highway',
+                        elementType: 'geometry.stroke',
+                        stylers: [{ color: '#1f2835' }],
+                    },
+                    {
+                        featureType: 'road.highway',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#f3d19c' }],
+                    },
+                    {
+                        featureType: 'transit',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#2f3948' }],
+                    },
+                    {
+                        featureType: 'transit.station',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#d59563' }],
+                    },
+                    {
+                        featureType: 'water',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#17263c' }],
+                    },
+                    {
+                        featureType: 'water',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#515c6d' }],
+                    },
+                    {
+                        featureType: 'water',
+                        elementType: 'labels.text.stroke',
+                        stylers: [{ color: '#17263c' }],
+                    },
+                ],
             };
 
             const map = new Map(mapRef.current as HTMLDivElement, options);
@@ -161,6 +240,8 @@ export default function GoogleMaps() {
 
                 marker.addListener('click', () => {
                     setSelectedEvent(recommendation);
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(() => marker.setAnimation(null), 750);
                 });
             });
         };
@@ -173,7 +254,27 @@ export default function GoogleMaps() {
     return (
         <div>
             {loading ? (
-                <div>Loading...</div>
+                <div className="flex items-center justify-center h-[calc(100vh-56px)] w-full">
+                    <div role="status">
+                        <svg
+                            aria-hidden="true"
+                            className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                            />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
             ) : (
                 <div
                     ref={mapRef}
@@ -194,21 +295,22 @@ export default function GoogleMaps() {
                     bottom: 0,
                     left: 0,
                     width: '100%',
-                    backgroundColor: '#fff',
-                    padding: '10px 20px',
-                    boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)',
+                    backgroundColor: '#2b2b2b',
+                    color: '#fff',
+                    padding: '15px 20px',
+                    boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.3)',
                     borderRadius: '10px 10px 0 0',
                     transition: 'all 0.3s ease-in-out'
                 }}>
                     <h3 style={{ margin: 0, fontSize: '18px', fontWeight: 'bold' }}>{selectedEvent.title}</h3>
-                    <p style={{ margin: '5px 0', fontSize: '14px', color: '#555' }}>{selectedEvent.venue}</p>
-                    <p style={{ margin: '5px 0', fontSize: '14px', color: '#777' }}>{selectedEvent.date}</p>
+                    <p style={{ margin: '5px 0', fontSize: '14px', color: '#ccc' }}>{selectedEvent.venue}</p>
+                    <p style={{ margin: '5px 0', fontSize: '14px', color: '#aaa' }}>{selectedEvent.date}</p>
                     <button
                         onClick={() => window.open(selectedEvent.ticketLink, '_blank')}
                         style={{
                             marginTop: '10px',
                             padding: '10px 20px',
-                            backgroundColor: '#007bff',
+                            backgroundColor: '#ff6347',
                             color: '#fff',
                             border: 'none',
                             borderRadius: '5px',
