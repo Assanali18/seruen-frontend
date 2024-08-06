ed48b12ddab2eabfd1332aac0d79db654458447c
Assanali18 <ustaevasanali@gmail.com>
2024-08-06 13:50:34 +0500

    ui 2

--- client/src/app/components/GoogleMaps.tsx
+++ client/src/app/components/GoogleMaps.tsx
@@ -107,7 +107,6 @@ export default function GoogleMaps() {
                                     return null;
                                 })
                             );
-
                             const uniquePositions = positions.reduce((acc, pos) => {
                                 if (pos) {
                                     const key = `${pos.lat},${pos.lng}`;
@@ -256,7 +255,7 @@ export default function GoogleMaps() {
 
                         marker.addListener('click', () => {
                             setSelectedEvent(recommendation);
-                            // Убрана анимация
+                            // Removing bounce animation
                         });
                     });
                 } catch ( error) {
@@ -273,19 +272,32 @@ export default function GoogleMaps() {
             {loading ? (
                 <div className="flex flex-col items-center justify-center h-[calc(100vh-56px)] w-full">
                     <div className="flex items-center justify-center">
-                        <svg className="animate-spin h-10 w-10 mr-3" viewBox="0 0 24 24">
-                            {/* Path for SVG */}
+                        <svg
+                            aria-hidden="true"
+                            className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                            />
                         </svg>
                         <span className="sr-only">Loading...</span>
                     </div>
-                    <p className="text-gray-500 text-center mt-2">
-                        Загрузка данных... Пожалуйста, включите геоданные.
+                    <p className="text-gray-500 text-sm text-center mt-2">
+                        Если у вас Android, пожалуйста, включите геоданные.
                     </p>
                 </div>
             ) : noRecommendations ? (
                 <div className="flex items-center justify-center h-[calc(100vh-56px)] w-full">
                     <p className="text-white text-lg font-bold">
-                        У вас пока нет рекомендаций. Пожалуйста, подождите, пока они загрузятся.
+                        У вас пока нет рекомендаций. Пожалуйста, подождите, пока они загрузятся. Для этого запустите /start и введите все данные.
                     </p>
                 </div>
             ) : geoPermissionDenied ? (
@@ -295,27 +307,50 @@ export default function GoogleMaps() {
                     </p>
                 </div>
             ) : (
-                <div ref={mapRef} className="absolute top-0 left-0 right-0 bottom-0 w-full h-[calc(100vh-56px)]" />
+                <div
+                    ref={mapRef}
+                    className="h-[calc(100vh-56px)] w-full"
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        right: 0,
+                    }}
+                />
             )}
 
             {selectedEvent && (
-                <div className={`fixed bottom-10 left-1/2 transform -translate-x-1/2 w-auto bg-gray-800 text-white p-4 rounded-lg shadow-lg transition-opacity duration-300 ${selectedEvent ? 'opacity-100' : 'opacity-0 invisible'}`}>
-                    <button
-                        onClick={() => setSelectedEvent(null)}
-                        className="absolute top-1 right-1 text-lg"
-                        style={{ color: '#fff' }}
-                    >
-                        &times;
-                    </button>
-                    <h3 className="text-lg font-bold">{selectedEvent.title}</h3>
-                    <p className="text-sm">{selectedEvent.venue}</p>
-                    <p className="text-sm">{selectedEvent.date}</p>
-                    <button
-                        onClick={() => window.open(selectedEvent.ticketLink, '_blank')}
-                        className="mt-2 py-2 px-4 bg-red-500 rounded text-white cursor-pointer text-sm"
-                    >
-                        Подробнее
-                    </button>
+                <div style={{
+                    position: 'fixed',
+                    bottom: '50px', // Adjusted for better visibility
+                    left: '50%', // Center align
+                    transform: 'translateX(-50%)', // Center align
+                    width: '90%', // Adjust width
+                    backgroundColor: '#2b2b2b',
+                    color: '#fff',
+                    padding: '20px',
+                    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                    borderRadius: '10px',
+                    transition: 'all 0.3s ease-in-out'
+                }}>
+                    <div className="relative flex flex-col items-start">
+                        <button
+                            onClick={() => setSelectedEvent(null)}
+                            className="absolute top-2 right-2 text-white text-xl"
+                        >
+                            &times;
+                        </button>
+                        <h3 className="text-xl font-bold mb-1">{selectedEvent.title}</h3>
+                        <p className="text-md mb-1">{selectedEvent.venue}</p>
+                        <p className="text-md mb-4">{selectedEvent.date}</p>
+                        <button
+                            onClick={() => window.open(selectedEvent.ticketLink, '_blank')}
+                            className="py-2 px-4 bg-red-500 hover:bg-red-700 text-white font-bold rounded shadow"
+                        >
+                            Подробнее
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
